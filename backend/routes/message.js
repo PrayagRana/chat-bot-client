44e60299ad5ca56602e17d63ba6734f2c44498e7
PrayagRana <56391007+PrayagRana@users.noreply.github.com>
2020-10-23 17:20:47 +0530

    Update message.js

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 let clientMessage = require('../model/message.model');
+const axios=require("axios");
+const { ObjectId } = require('bson');
 
 router.route('/').get((req, res) => {
   clientMessage.find()
@@ -7,26 +9,107 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-  
-  const email=req.body.email;
-  const emailClient=req.body.emailClient;
-  const Message = req.body.message;
-  
-  const newMessage = new Message({    
-      
+router.route('/clientCustomerMessage').post((req, res) => {
+  const id=req.body.emailClient+req.body.emailClientCustomer;
+  const message=req.body.emailClientCustomer+': '+req.body.message;  
+     
+ 
 
-      email,
-      emailClient,
-      message
-      
+  
     
+     clientMessage.findOneAndUpdate({
+    _id:id
+    },{$push:{Message:message}}, (
+      err,
+      result
+    )=> {
+      if (err) {
+        res.send(err);
+      } 
+      else if (result===null)
+      {
+      
+        const _id=id;
+        var  Message=[];
+        Message.push(message);
+        
+        const newMessageArray = new clientMessage({
+       _id,        
+       Message 
+      });
+      newMessageArray.save()
+      .then(() => res.json('Message added!'))
+      .catch(err => res.status(400).json('Error: ' + err));
+        
+      }
+      else
+      {
+        
+        res.send(result);
+      }
     });
+    
+    
+  
 
   
-  newMessage.save()
-    .then(() => res.json('Message added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+
+
+});
+router.route('/clientMessage').post((req, res) => {
+  const id=req.body.emailClient+req.body.emailClientCustomer;
+  const message=req.body.emailClient+': '+req.body.message;  
+     
+      clientMessage.findOneAndUpdate({
+     _id:id
+     },{$push:{Message:message}}, (
+       err,
+       result
+     )=> {
+       if (err) {
+         res.send(err);
+       } 
+       else if (result===null)
+       {
+       
+         const _id=id;
+         
+         
+         
+          
+           var  Message=[];
+ 
+         Message.push(message);
+         
+         const newMessageArray = new clientMessage({
+        _id,        
+        Message 
+       });
+       newMessageArray.save()
+       .then(() => res.json('Message added!'))
+       .catch(err => res.status(400).json('Error: ' + err));
+         
+       }
+       else
+       {
+         
+         res.send(result);
+       }
+     });
+     
+     
+   
+ 
+   
+ 
+ 
+ });
+
+
+router.route("/:id").get((req, res) => {
+  clientMessage.findById(req.params.id)
+    .then((message) => res.json(message))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
