c93b75b7c142c4335d7a60fe2b93ddc3da8e9bcc
PrayagRana <56391007+PrayagRana@users.noreply.github.com>
2020-10-23 17:21:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
+try{
 require("dotenv").config();
 
 const app = express();
@@ -11,10 +11,12 @@ const port = process.env.port || 4000;
 app.use(cors());
 app.use(express.json());
 
+
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateindex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateindex: true,useFindAndModify: false });
 
 const connection = mongoose.connection;
+
 
 connection.once("open", () =>{
   console.log("you are connceted to your mongo database");
@@ -29,3 +31,8 @@ app.use("/message", message);
 
 
 app.listen(port, () => console.log(`your server is running on port: ${port}`));
+
+}catch(error)
+{
+  console.log(error);
+}
